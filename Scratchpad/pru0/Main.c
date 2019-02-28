518e2867834dec1dbb62cffa4898055169f96a4a
SNGomashie <s.gomashie@hotmail.nl>
2019-02-28 11:57:43 +0100

    added debug lights

--- Scratchpad/pru0/Main.c
+++ Scratchpad/pru0/Main.c
@@ -7,18 +7,29 @@
 #define RAND_MAX 0xFFFF;
 
 typedef struct {
-	uint32_t reg0;
-	uint32_t reg1;
-	uint32_t reg2;
-	uint32_t reg3;
+	uint32_t reg6;
+	uint32_t reg7;
+	uint32_t reg8;
+	uint32_t reg9;
 } bufferData;
 
 bufferData dmemBuf;
 
 #define PRU1_PRU0_INTERRUPT (20)
 
-volatile register uint32_t __R30;
-volatile register uint32_t __R31;
+
+#define GPIO1 0x4804C000
+#define GPIO_CLEARDATAOUT 0x190
+#define GPIO_SETDATAOUT 0x194
+#define USR0 (1<<21)
+#define USR1 (1<<22)
+#define USR2 (1<<23)
+#define USR3 (1<<24)
+unsigned int volatile * const GPIO1_CLEAR = (unsigned int *) (GPIO1 + GPIO_CLEARDATAOUT);
+unsigned int volatile * const GPIO1_SET   = (unsigned int *) (GPIO1 + GPIO_SETDATAOUT);
+
+volatile register unsigned int __R30;
+volatile register unsigned int __R31;
 
 #define SHARE_MEM  0x00010000
 volatile uint32_t *shared =  (unsigned int *) SHARE_MEM;
@@ -33,15 +44,19 @@ void main(void) {
   CT_INTC.SECR0 = 0xFFFFFFFF;
   CT_INTC.SECR1 = 0xFFFFFFFF;
 
-  dmemBuf.reg0 = (0xFFFF << 4);
-  dmemBuf.reg1 = (0xF0FF << 4);
-  dmemBuf.reg2 = 0x0000FF0F;
-  dmemBuf.reg3 = 0x0000FFF0;
+  dmemBuf.reg0 = 0xFFFFFFFF;
+  dmemBuf.reg1 = 0xFFFFFFFF;
+  dmemBuf.reg2 = 0xFFFFFFFF;
+  dmemBuf.reg3 = 0xFFFFFFFF;
+
+	*GPIO1_CLEAR = USR3; //turn off light
 
   while ((__R31 & (1<<30)) == 0) {
   }
 	buf = dmemBuf;
-  __xout(14, 0, 0, buf);
+  __xout(14, 6, 0, buf);
+
+	*GPIO1_SET = USR3; //turn on light
 
   CT_INTC.SICR = PRU1_PRU0_INTERRUPT;
 
