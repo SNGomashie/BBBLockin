ca0881ea03165b4cb97f910846372e071042c933
SNGomashie <s.gomashie@hotmail.nl>
2019-03-01 14:55:35 +0100

    testing clock

--- Bit-banged_ADC/pru0/Main.c
+++ Bit-banged_ADC/pru0/Main.c
@@ -17,117 +17,35 @@
 #include <pru_cfg.h>
 #include "resource_table.h"
 
-/* PRU definiton */
-#define PRU0
-
-/* Data object to be send through the scratchpad */
-typedef struct {
-	uint16_t reg0;
-	uint16_t reg1;
-	uint16_t reg2;
-	uint16_t reg3;
-} bufferData;
-
-bufferData dmemBuf;
-
 //Define pin locations
-#define NRD 0b10000000
-#define CS 0b100000
-#define MISO 0b1000
-#define MOSI 0b10
-#define CLK 0b100
-#define CONVST 0b1
+#define NRD 7
+#define CS 5
+#define MISO 3
+#define MOSI 1
+#define CLK 2
+#define CONVST 0
 
 /* GPIO registers */
 volatile register uint32_t __R30;
 volatile register uint32_t __R31;
 
-/* Shared memory location & definiton*/
-#define SHARE_MEM  0x00010000
-#define PRU0_MEM 0x00000000
-volatile uint32_t *shared =  (unsigned int *) SHARE_MEM;
-volatile uint32_t *pru0_mem =  (unsigned int *) PRU0_MEM;
-
-/* Interrupt definitions */
-#define INT_OFF 0x00000000
-#define INT_ON 0xFFFFFFFF
-
-/* Number of sample channels */
-const uint8_t Nchl = 4;
-uint8_t idx;
-uint8_t i;
-
-/* SPI data */
-uint16_t spiCommand = 0x00000000;
-uint16_t spiReceive = 0x00000000;
-
-uint16_t fnRead_WriteSPI(uint8_t chan);
-
 void main(void) {
-	shared[0] = INT_OFF;
-	/* 1.	Initialization */
-                              //  The data out line is connected to R30 bit 1.
-	__R30 = 0x00000000;         //  Clear the output pin.
-	__R31 = 0x00000000;		  //  Clear the input pin.
-	__R30 = __R30 | ( 0 << CS );  // Initialize chip select LOW.
-	__R30 = __R30 | ( 1 << NRD ); // Initialize Read input LOW.
-	__R30 = __R30 | ( 1 << CONVST ); //Initialize conversion start HIGH.
+    //  The data out line is connected to R30 bit 1.
+  __R30 = 0x00000000;         //  Clear the output pin.
+  __R31 = 0x00000000;		  //  Clear the input pin.
+  __R30 |= (0 << CS);  // Initialize chip select LOW.
+  __R30 |= (0 << NRD); // Initialize Read input LOW.
+  __R30 |= (1 << CONVST); //Initialize conversion start HIGH.
 
-/* Infinite loop */
-	while(1) {
-		while(shared[0] == INT_OFF){
-			/* Fill the struct with 16 bit random values */
-			dmemBuf.reg0 = fnRead_WriteSPI(0);
-			dmemBuf.reg1 = fnRead_WriteSPI(1);
-			dmemBuf.reg2 = fnRead_WriteSPI(2);
-			dmemBuf.reg3 = fnRead_WriteSPI(3);
-
-			/* Send interrupt over shared memory */
-			shared[0] = INT_ON;
-
-			/* Send data object through the scratchpad */
-			__xout(10, 0, 0, dmemBuf);
-
-			/* Delay for a second */
-			__delay_cycles(200000000);
-		}
-	}
+  __R30 |= (0 << CLK); //Clock polarity 0
+while (1){
+  for (i = 0; i < 16; i++){ //Create a clock pulse for every received and send bit
+    __R30 |= (1 << CLK);
+    __delay_cycles(25);
+    __R30 |= (0 << CLK);
+  }
+  __delay_cycles(2000000);
 }
 
-uint16_t fnRead_WriteSPI(uint8_t chan){
-const uint8_t ADCch[] = {0, 4, 1, 5, 2, 6, 3, 7};
 
-spiCommand = ( ADCch[chan] << 4 ) | 0b10000000;	// single-ended, input +/-5V
-
-__R30 = __R30 | ( 1 << CS ); //Set CS high
-__R30 = __R30 | ( 0 << NRD ); //Set nRD low
-__R30 = __R30 | ( 0 << CONVST ); //Set ConvST low
-
-__R30 &= ~(1 << CLK); //Clock polarity 0
-
-for (i = 0; i < 15; i++) { //Create a clock pulse for every received and send bit
-	spiReceive = spiReceive << 1; //shift
-
-	if (( spiCommand << i ) & 0x80){ //
-		__R30 |= ( 1 << MOSI );
-	} else {
-		__R30 &= ~( 1 << MOSI );
-	}
-
-	__R30 ^= ( 1 << CLK ); //Rising edge Γ
-
-	if (__R31 & ( 1 << MISO )) {
-		spiReceive |= 0x01;
-	} else {
-		spiReceive &= ~(0x01);
-	}
-
-	__R30 ^= ( 1 << CLK ); //Falling edge Լ
-}
-
-__R30 = __R30 | ( 1 << NRD );
-__R30 = __R30 | ( 1 << CONVST );
-__R30 = __R30 | ( 0 << CS );
-
-return spiReceive;
 }
