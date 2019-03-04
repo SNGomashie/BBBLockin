c27414cd5c9b5ccd9546ebd10765e355fe4714e7
SNGomashie <s.gomashie@hotmail.nl>
2019-03-04 17:24:37 +0100

    timing fix

--- Bit-banged_ADC/pru0/Main.c
+++ Bit-banged_ADC/pru0/Main.c
@@ -94,26 +94,47 @@ uint16_t fnRead_WriteSPI(uint8_t chan){
 	__R30 &= ~(1 << NRD); //Set nRD low
 	__R30 &= ~(1 << CLK); //Set CLK low
 
-	for (i = 0; i < 16; i++){ //Loop for every clock pulse
-		spiReceive = spiReceive << 1; //shift
+do{
+	spiReceive = spiReceive << 1; //shift
 
-		if (spiCommand & (1 << i)){//write the command
-			__R30 |= (1 << MOSI);
-		}else{
-			__R30 &= ~(1 << MOSI);
-		}
-
-		__R30 &= ~(1 << CLK);//Rising edge Γ
-
-		if (__R31 & (1 << MISO)){//Save MISO
-			spiReceive |= 0x01;
-		}else{
-			spiReceive &= ~(0x01);
-		}
-
-		__R30 |= (1 << CLK);//Falling edge Լ
-
+	if (spiCommand & (1 << i)){//write the command
+		__R30 |= (1 << MOSI);
+	}else{
+		__R30 &= ~(1 << MOSI);
 	}
+
+	__R30 &= ~(1 << CLK);//Rising edge Γ
+
+	if (__R31 & (1 << MISO)){//Save MISO
+		spiReceive |= 0x01;
+	}else{
+		spiReceive &= ~(0x01);
+	}
+
+	__R30 |= (1 << CLK);//Falling edge Լ
+	i ++
+}while(i < 16);
+
+	// for (i = 0; i < 16; i++){ //Loop for every clock pulse
+	// 	spiReceive = spiReceive << 1; //shift
+	//
+	// 	if (spiCommand & (1 << i)){//write the command
+	// 		__R30 |= (1 << MOSI);
+	// 	}else{
+	// 		__R30 &= ~(1 << MOSI);
+	// 	}
+	//
+	// 	__R30 &= ~(1 << CLK);//Rising edge Γ
+	//
+	// 	if (__R31 & (1 << MISO)){//Save MISO
+	// 		spiReceive |= 0x01;
+	// 	}else{
+	// 		spiReceive &= ~(0x01);
+	// 	}
+	//
+	// 	__R30 |= (1 << CLK);//Falling edge Լ
+	//
+	// }
 
 	__R30 |= ( 1 << NRD ); //Set nRD high
 	__R30 |= ( 1 << CONVST ); //Set convST high
