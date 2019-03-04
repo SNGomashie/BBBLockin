e8bdb037cbe1c7d44a16151761974f08f90a232e
SNGomashie <s.gomashie@hotmail.nl>
2019-03-04 10:55:37 +0100

    fix

--- Bit-banged_ADC/pru0/Main.c
+++ Bit-banged_ADC/pru0/Main.c
@@ -10,12 +10,12 @@
 //  Convst (  Start conversion ) : 	P9.31 pr1_pru0_pru_r30_0
 
 //Define pin locations
-#define NRD 0x00000080
-#define CS 0x00000020
-#define MISO 0x00000008
-#define MOSI 0x00000002
-#define CLK 0x00000004
-#define CONVST 0x00000001
+#define NRD 7
+#define CS 5
+#define MISO 3
+#define MOSI 1
+#define CLK 2
+#define CONVST 0
 
 #include <stdint.h>
 #include <pru_cfg.h>
@@ -31,11 +31,10 @@ void main(void)
 	/* Clear SYSCFG[STANDBY_INIT] to enable OCP master port */
 	CT_CFG.SYSCFG_bit.STANDBY_INIT = 0;
 
-	__R30 = 0x00000000;
-
-	/* TODO: Create stop condition, else it will toggle indefinitely */
-	while (1) {
-		__R30 ^= CLK;
-		__delay_cycles(5000000);
-	}
+	__R30 = 0x00000000;         //  Clear the output pin.
+	__R31 = 0x00000000;		  //  Clear the input pin.
+	__R30 |= (0 << CS);  // Initialize chip select LOW.
+	__R30 |= (0 << NRD); // Initialize Read input LOW.
+	__R30 |= (1 << CONVST); //Initialize conversion start HIGH.
+	__R30 |= (0 << CLK);
 }
