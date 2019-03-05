1d5a3ebbab01e08200e5fde795e32c9ce49643ce
SNGomashie <s.gomashie@hotmail.nl>
2019-03-05 10:21:39 +0100

    delay

--- Bit-banged_ADC/pru0/Main.c
+++ Bit-banged_ADC/pru0/Main.c
@@ -73,6 +73,7 @@ void main(void)
 			__R30 |= (1 << CONVST); //Initialize conversion start HIGH.
 			/* Fill the struct with 16 bit adc values */
 			dmemBuf.reg0 = fnRead_WriteSPI(0);
+			__delay_cycles(1000);
 			dmemBuf.reg1 = fnRead_WriteSPI(2);
 
 			/* Send interrupt over shared memory */
@@ -89,8 +90,8 @@ void main(void)
 uint16_t fnRead_WriteSPI(uint8_t chan){
 	const uint8_t ADCch[] = {0, 4, 1, 5, 2, 6, 3, 7};
 
-	spiCommand = ( ADCch[chan] << 4 ) | 0b10001000;	// single-ended, input +/-5V
-
+	spiCommand = ( ADCch[chan] << 4 ) | 0b10001000;	// single-ended, input +5V
+	//spiCommand = (ADCch[chan] << 4) | B10000000; // single-ended, input +/-5V
 	__R30 |= (1 << CS); //Set CS high
 	__R30 &= ~(1 << CONVST); //Set ConvST low
 	__R30 &= ~(1 << NRD); //Set nRD low
