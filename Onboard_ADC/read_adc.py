e27a12b0cb0cec723c6281c64527425116efef28
SNGomashie <s.gomashie@hotmail.nl>
2019-03-05 17:01:55 +0100

    clean up

--- Onboard_ADC/read_adc.py
+++ Onboard_ADC/read_adc.py
@@ -4,14 +4,6 @@ import os
 import argparse
 import time
 
-def bytes_to_int(bytes):
-    result = 0
-
-    for b in bytes:
-        result = result * 256 + int(b)
-
-    return result
-
 def readADCchannel(adcChannel):
     outputFile = "/dev/rpmsg_pru30"
     try:
