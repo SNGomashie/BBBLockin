e66098e6f73e021c8f9cd9d2bba5c32c2b410bb1
SNGomashie <s.gomashie@hotmail.nl>
2019-03-08 11:04:56 +0100

    fix

--- McSPI/Main.c
+++ McSPI/Main.c
@@ -58,6 +58,8 @@ void main(void){
   //Write word to transmit
   CT_MCSPI0.TX0 = 0x8801;
 
+  // Disable channel
+  CT_MCSPI0.CH0CTRL_bit.EN = 0x0;
   //Wait until interrupt
   while((__R31 & (0x1<<30))==0) {		// Wait for PRU 0
 		}
@@ -66,8 +68,7 @@ void main(void){
 
   __R30 |= (1 << CONVST);
 
-  // Disable channel
-  CT_MCSPI0.CH0CTRL_bit.EN = 0x0;
+
 
   __halt();
 }
