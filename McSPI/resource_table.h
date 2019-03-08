cbc41194ecc435b15a8358fa2d66e99175da1b11
SNGomashie <s.gomashie@hotmail.nl>
2019-03-08 09:29:23 +0100

    fix

--- McSPI/resource_table.h
+++ McSPI/resource_table.h
@@ -63,7 +63,7 @@
 #define HOST_UNUSED		255
 
 /* Mapping sysevts to a channel. Each pair contains a sysevt, channel */
-struct ch_map pru_intc_map[] = { {44, 1},
+struct ch_map pru_intc_map[] = { {44, 1}, {44, 0}
 };
 
 struct my_resource_table {
