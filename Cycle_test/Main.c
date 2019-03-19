181a9a210cedbe979d172b828e973556b36f242c
SNGomashie <s.gomashie@hotmail.nl>
2019-03-19 14:19:07 +0100

    fix

--- Cycle_test/Main.c
+++ Cycle_test/Main.c
@@ -6,26 +6,26 @@
 
 #define out 1       // Bit number to output on
 
-volatile register uint32_t R30;
-volatile register uint32_t R31;
+volatile register unsigned int __R30;
+volatile register unsigned int __R31;
 
 void main(void)
 {
     // These will be kept in registers and never witten to DRAM
-    uint32_t cycle, stall;
+    uint32_t cycle;
 
     // Clear SYSCFG[STANDBY_INIT] to enable OCP master port
     CT_CFG.SYSCFG_bit.STANDBY_INIT = 0;
 
     PRU0_CTRL.CTRL_bit.CTR_EN = 1;  // Enable cycle counter
 
-    uint8_t a = 10
-    uint8_t b = 5
-    uint8_t c = 0
+    uint8_t a = 10;
+    uint8_t b = 5;
+    uint8_t c = 0;
     // Reset cycle counter, cycle is on the right side to force the compiler
     // to put it in it's own register
     PRU0_CTRL.CYCLE = cycle;
-    c = a * b
+    c = a * b;
     cycle = PRU0_CTRL.CYCLE;    // Read cycle and store in a register
     __halt();
 }
