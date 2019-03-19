824648e49209769a3767dfc635e96bab04ec0cd8
SNGomashie <s.gomashie@hotmail.nl>
2019-03-19 17:16:31 +0100

    fix

--- README.md
+++ README.md
@@ -9,6 +9,8 @@ During my intership at SRON Netherlands Institute for Space Research I had to de
 - [TI AM335x Technical Reference Manual](https://www.ti.com/lit/ug/spruh73p/spruh73p.pdf)
 - [TI PRU Optimizing C/C++ Compiler](http://www.ti.com/lit/ug/spruhv7c/spruhv7c.pdf)
 - [TI PRU Read Latencies](http://www.ti.com/lit/an/sprace8a/sprace8a.pdf)
+- [TI Hands-on labs](http://processors.wiki.ti.com/index.php/PRU_Training:_Hands-on_Labs)
+- [Old Elinux entry](https://elinux.org/Ti_AM33XX_PRUSSv2)
 
 
 ## Table of contents
--- Sync/Main.c
+++ Sync/Main.c
@@ -11,21 +11,48 @@
 volatile register uint32_t __R30;
 volatile register uint32_t __R31;
 
+#define PRU0_MEM 0x00000000
+volatile uint32_t *pru0_mem =  (unsigned int *) PRU0_MEM;
+
+void initIEP(void);
+
 void main(void)
 {
     // These will be kept in registers and never witten to DRAM
-    uint32_t cycle;
+    uint32_t cycle, period;
 
     // Clear SYSCFG[STANDBY_INIT] to enable OCP master port
     CT_CFG.SYSCFG_bit.STANDBY_INIT = 0;
 
-    PRU0_CTRL.CTRL_bit.CTR_EN = 1;  // Enable cycle counter
 
 
+    // Using the IEP counter external to the PRU
+    initIEP();
     while (!(__R31 & (1 << SYNC)));
-      PRU0_CTRL.CYCLE = cycle;
-      while (__R31 & (1 << SYNC));
-      while(!(__R31 & (1 << SYNC)));
-      cycle = PRU0_CTRL.CYCLE;    // Read cycle and store in a register
+    CT_IEP.TMR_GLB_CFG_bit.CNT_EN = 0x1;  // Enable counter
+    while (__R31 & (1 << SYNC));
+    while(!(__R31 & (1 << SYNC)));
+    pru0_mem[0] = CT_IEP.TMR_CNT;    // Read cycle and store in a register
+
+    // Using the cycle counter internal to the PRU
+
+    // PRU0_CTRL.CTRL_bit.CTR_EN = 1;  // Enable cycle counter
+    // while (!(__R31 & (1 << SYNC)));
+    //   PRU0_CTRL.CYCLE = cycle;
+    //   while (__R31 & (1 << SYNC));
+    //   while(!(__R31 & (1 << SYNC)));
+    //   cycle = PRU0_CTRL.CYCLE;    // Read cycle and store in a register
     __halt();
+}
+
+void initIEP (void){
+  /* Disable counter */
+  CT_IEP.TMR_GLB_CFG_bit.CNT_EN = 0x0;
+
+  /* Clear CNT register */
+  CT_IEP.TMR_CNT = 0xFFFFFFFF;
+
+  /* Clear overflow register */
+  CT_IEP.TMR_GLB_STS = 0x1;
+
 }
