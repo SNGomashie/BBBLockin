7faa107f878e11b2956b1b503aae10b625f63153
SNGomashie <s.gomashie@hotmail.nl>
2019-03-26 15:21:39 +0100

    Cleaned everything up

--- Sim/SimNoise.py
+++ Sim/SimNoise.py
@@ -1,92 +1,66 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy import signal
-
-
-def int_sqrt32(x):
-    res = 0
-    add = 0x8000
-    for i in range(16):
-        temp = res | add
-        g2 = temp * temp
-        if x >= g2:
-            res = temp
-        add = add >> 1
-    return res
-
+from scipy import signal, fftpack
 
 # Frequncies and periods
-Fs = 4800  # Hz
+Fs = 10000  # Hz
 Ts = 1 / Fs  # s
 Fr = 100  # Hz
 Tr = 1 / Fr  # s
 # Constants
 Ar = 1  # V
-Ai = 5  # V
-P = 100  # Periods
+Ai = 1.8  # V
+P = 1000  # Periods
 a = 13.219  # Randomness
+
 
 # Samples
 T = P * Tr  # periods
 n = int(T * Fs)
 t = np.linspace(0, T, n, endpoint=False)
-Ao = np.zeros(len(t))
-
-# Quantizition
-quant_bits = 16
-quant_levels = np.power(2, quant_bits) / 2
-quant_step = 1 / quant_levels
 
 # Experimental signal
 VsigCos = Ai * np.cos((t * 2 * np.pi * Fr) - (np.pi / 10))
-# Added phase difference
-
-# Reference signal(square wave) ( Also uncomment Ao)
-# VrefCos = Ar * signal.square( n * 2 * np.pi * fo)
-# VrefSin = Ar * signal.square(( n * 2 * np.pi * fo) - (np.pi/2))
 
 # Reference signals(sinus and cosinus)
 VrefCos = Ar * np.cos(t * 2 * np.pi * Fr)
 VrefSin = Ar * np.cos((t * 2 * np.pi * Fr) - (np.pi / 2))
 
+# Reference signal(square wave) ( Also uncomment Ao)
+VrefCossqr = Ar * signal.square(t * 2 * np.pi * Fr)
+VrefSinsqr = Ar * signal.square((t * 2 * np.pi * Fr) - (np.pi/2))
+
 # Noise generation
 np.random.seed(45)
-Expnoise = a * np.random.randn(len(t), 1)
+Noise = a * np.random.randn(len(t))
 
 # SNR
-npwr = np.sum(np.power(Expnoise, 2))
-sigpwr = np.sum(np.power(VsigCos, 2))
+npwr = sum(np.power(Noise, 2))
+sigpwr = sum(np.power(VsigCos, 2))
+SNR = 10 * np.log10((sigpwr) / (npwr))
 
 # Input + Noise
-VsigCosandNoise = VsigCos + Expnoise
-
-nyq = 0.5 * Fs
-Fc = 0.01 / nyq
-a = 1
-b, a = signal.butter(4, 0.0001, btype='low', analog=False)
+VsigCosandNoise = VsigCos + Noise
 
 # Multiply signal with reference Sin and Cos
 Vc = VsigCosandNoise * VrefCos
 Vs = VsigCosandNoise * VrefSin
 
+# Multiply signal with reference Sin and Cos without noise
+Vcnn = VsigCos * VrefCos
+Vsnn = VsigCos * VrefSin
+
 # calculate RMS
-# filtVc = signal.lfilter(b, a, Vs)
-# filtVs = signal.lfilter(b, a, Vc)
 Ivs = np.mean(Vs)
 Qvc = np.mean(Vc)
 
 # Find magnitude (Square wave)
 # Ao[k] = (np.pi/4) * 2 * np.sqrt((np.power(Ivs, 2))+(np.power(Qvc, 2)));
 
-# Find magnitude
-# if Ivs < Qvc:
-#     Ao = 2 * (0.96 * Qvc + 0.40 * Ivs)
-# elif Qvc < Ivs:
-#     Ao = 2 * (0.96 * Qvc + 0.40 * Ivs)
-# R = (Ivs * Ivs) + (Qvc * Qvc)
-# Ao = 2 * int_sqrt32(R)
+# Calculate magnitude of vector
 Ao = 2 * np.sqrt((np.power(Ivs, 2)) + (np.power(Qvc, 2)))
 
+# print refernce voltage and Lock-in output
 print("Input amplitude : %.3f V" % Ai)
 print("Output amplitude : %.6f V" % Ao)
 
@@ -94,13 +68,105 @@ print("Output amplitude : %.6f V" % Ao)
 Phase = np.arctan2(Ivs, Qvc)
 print("Phase difference : %.3f rads" % Phase)
 
-SNR = 10 * np.log10((sigpwr) / (npwr))
+# Print noise immunity
 Aerr = Ao - Ai
 print("SNR is : %.3f dB" % SNR)
 print("Amplitude error : %.6f V" % Aerr)
 
-plt.plot(t, VsigCos, 'r-')
+
+plt.subplot(2, 1, 1)
+plt.title("Experiment signal")
+plt.xlabel("Time [s]")
+plt.ylabel("Amplitude [V]")
+plt.grid(True)
+plt.xlim(0, 0.1)
+plt.plot(t, VsigCos)
+plt.subplot(2, 1, 2)
+plt.title("SYNC signal")
+plt.xlabel("Time [s]")
+plt.ylabel("Amplitude [V]")
+plt.grid(True)
+plt.xlim(0, 0.1)
+plt.plot(t, VrefCossqr)
+plt.tight_layout()
 plt.show()
-# plt.magnitude_spectrum(Ao, Fs=Fs, color='C1', scale='dB')
-# plt.magnitude_spectrum(VsigCosandNoise, Fs=Fs, color='C2', scale='dB')
+
+plt.subplot(3, 1, 1)
+plt.title("Experiment signal")
+plt.xlabel("Time [s]")
+plt.ylabel("Amplitude [V]")
+plt.grid(True)
+plt.xlim(0, 0.1)
+plt.plot(t, VsigCos)
+plt.subplot(3, 1, 2)
+plt.title("Reference cosinus")
+plt.xlabel("Time [s]")
+plt.ylabel("Amplitude [V]")
+plt.grid(True)
+plt.xlim(0, 0.1)
+plt.plot(t, VrefCos)
+plt.subplot(3, 1, 3)
+plt.title("Reference sinus")
+plt.xlabel("Time [s]")
+plt.ylabel("Amplitude [V]")
+plt.grid(True)
+plt.xlim(0, 0.1)
+plt.plot(t, VrefSin)
+plt.tight_layout()
+# plt.savefig('sigrefref.png')
+plt.show()
+
+plt.subplot(2, 1, 1)
+plt.title("Experiment signal without noise")
+plt.plot(t, VsigCos)
+plt.xlabel("Time [s]")
+plt.ylabel("Amplitude [V]")
+plt.xlim(0, 0.1)
+plt.subplot(2, 1, 2)
+plt.title("Experiment signal with noise")
+plt.plot(t, VsigCosandNoise)
+plt.xlabel("Time [s]")
+plt.ylabel("Amplitude [V]")
+plt.xlim(0, 0.1)
+plt.grid(True)
+plt.tight_layout()
+# plt.savefig('Vnoisenonoisetime.png')
+plt.show()
+
+plt.subplot(2, 1, 1)
+plt.title("Signal without noise")
+plt.magnitude_spectrum(VsigCos, Fs=Fs, color='C1', scale='dB')
+plt.xlim(0, 200)
+plt.ylim(-30, 0)
+plt.subplot(2, 1, 2)
+plt.title("Signal wth noise")
+plt.magnitude_spectrum(VsigCosandNoise, Fs=Fs, color='C2', scale='dB')
+plt.xlim(0, 200)
+plt.ylim(-30, 0)
+plt.tight_layout()
+# plt.savefig('Vnoisenonoisefreq.png')
+plt.show()
+
+plt.subplot(2, 2, 1)
+plt.title("Signal * Cos no noise")
+plt.magnitude_spectrum(Vcnn, Fs=Fs, color='C1', scale='dB')
+plt.xlim(-10, 210)
+plt.ylim(-30, 0)
+plt.subplot(2, 2, 2)
+plt.title("Signal * Sin no noise")
+plt.magnitude_spectrum(Vsnn, Fs=Fs, color='C2', scale='dB')
+plt.xlim(-10, 210)
+plt.ylim(-30, 0)
+plt.subplot(2, 2, 3)
+plt.title("Signal * Cos noise")
+plt.magnitude_spectrum(Vc, Fs=Fs, color='C3', scale='dB')
+plt.xlim(-10, 210)
+plt.ylim(-30, 0)
+plt.subplot(2, 2, 4)
+plt.title("Signal * Sin noise")
+plt.magnitude_spectrum(Vs, Fs=Fs, color='C4', scale='dB')
+plt.xlim(-10, 210)
+plt.ylim(-30, 0)
+plt.tight_layout()
+# plt.savefig('vcvs.png')
 plt.show()
