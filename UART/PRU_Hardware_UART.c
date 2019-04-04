42dbfd2b50a1fccdd162d2c15f9142337deb19f3
SNGomashie <s.gomashie@hotmail.nl>
2019-04-04 13:23:40 +0200

    fix

--- UART/PRU_Hardware_UART.c
+++ UART/PRU_Hardware_UART.c
@@ -1,8 +1,39 @@
+/*
+ * Copyright (C) 2015 Texas Instruments Incorporated - http://www.ti.com/
+ *
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *	* Redistributions of source code must retain the above copyright
+ *	  notice, this list of conditions and the following disclaimer.
+ *
+ *	* Redistributions in binary form must reproduce the above copyright
+ *	  notice, this list of conditions and the following disclaimer in the
+ *	  documentation and/or other materials provided with the
+ *	  distribution.
+ *
+ *	* Neither the name of Texas Instruments Incorporated nor the names of
+ *	  its contributors may be used to endorse or promote products derived
+ *	  from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 #include <stdint.h>
 #include <pru_uart.h>
-
-/* Define UART register "overlay" */
-volatile far pruUart CT_UART __attribute__((cregister("UART0", near), peripheral));
+#include "resource_table.h"
 
 /* The FIFO size on the PRU UART is 16 bytes; however, we are (arbitrarily)
  * only going to send 8 at a time */
@@ -18,7 +49,8 @@ struct {
 /* Making this buffer global will force the received data into memory */
 uint8_t buffer[MAX_CHARS];
 
-void main(){
+void main(void)
+{
 	uint8_t tx;
 	uint8_t cnt;
 
@@ -43,12 +75,13 @@ void main(){
 	 * FIFOs by writing to FCR. FIFOEN bit in FCR must be set first before
 	 * other bits are configured */
 	/* Enable FIFOs for now at 1-byte, and flush them */
-	CT_UART.FCR = (0x4) | (0x2) | (0x1);
+	CT_UART.FCR = (0x8) | (0x4) | (0x2) | (0x1);
 	//CT_UART.FCR = (0x80) | (0x4) | (0x2) | (0x01); // 8-byte RX FIFO trigger
 
 	/* Choose desired protocol settings by writing to LCR */
 	/* 8-bit word, 1 stop bit, no parity, no break control and no divisor latch */
 	CT_UART.LCR = 3;
+	CT_UART.LCR_bit.DLAB = 0;
 
 	/* Enable loopback for test */
 	CT_UART.MCR = 0x10;
@@ -72,7 +105,7 @@ void main(){
 	/*** SEND SOME DATA ***/
 
 	/* Let's send/receive some dummy data */
-	for (cnt = 0; cnt < MAX_CHARS; cnt++){
+	for (cnt = 0; cnt < MAX_CHARS; cnt++) {
 		/* Load character, ensure it is not string termination */
 		if ((tx = hostBuffer.data[cnt]) == '\0')
 			break;
