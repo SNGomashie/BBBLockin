dbe21552a9fd83d545be74ccae1721e244fae6b1
SNGomashie <s.gomashie@hotmail.nl>
2019-04-10 17:01:29 +0200

    fix

--- DDS/Main.c
+++ DDS/Main.c
@@ -75,7 +75,7 @@ void main(void){
       __R30 ^= 1 << PIN;
 
       /* Format string to be send */
-      sprintf(data,"%d, %d, %d\n", sinLUT64[accumulator >> 26], (accumulator >> 26), period);
+      sprintf(data,"%d, %d, %x\n", sinLUT64[accumulator >> 26], (accumulator >> 26), period);
       // sprintf(data, "%x %x\n", accumulator, period);
 
       /* Print to serial port */
