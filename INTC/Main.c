4b454325e3aacb019018dc77b407b611cf594547
SNGomashie <s.gomashie@hotmail.nl>
2019-04-11 16:49:48 +0200

    fix

--- INTC/Main.c
+++ INTC/Main.c
@@ -30,12 +30,17 @@ void main(void){
 
   while(1){
     while (__R31 & HOST_INT){
+      /* Clear Compare status */
+      CT_IEP.TMR_CMP_STS = (1 << 0);
+
+      /* delay for 5 cycles, clearing takes time */
+      __delay_cycles(5);
+
       /* Clear the status of the interrupt */
       CT_INTC.SICR = 7;
-      /* delay */
-      __delay_cycles(4);
-      /* Clear Compare status */
-      CT_IEP.TMR_CMP_STS |= (1 << 0);
+
+      /* delay for 5 cycles, clearing takes time */
+      __delay_cycles(5);
 
       /* Toggle pin */
       __R30 ^= 1 << PIN25;
