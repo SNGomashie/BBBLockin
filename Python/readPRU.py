0316636358b5b594654664652efe8dce2874a02f
SNGomashie <s.gomashie@hotmail.nl>
2019-04-16 15:35:10 +0200

    fix

--- Python/readPRU.py
+++ Python/readPRU.py
@@ -2,7 +2,6 @@
 
 import sys
 
-
 # Character device PRU0
 CHAR_DEV0 = "/dev/rpmsg_pru30"
 
@@ -10,42 +9,46 @@ CHAR_DEV0 = "/dev/rpmsg_pru30"
 REMOTEPROC_STATE0 = "/sys/class/remoteproc/remoteproc1/state"
 REMOTEPROC_FIRM0 = "/sys/class/remoteproc/remoteproc1/firmware"
 
-# Initialize PRUs
-PRUstate = open(REMOTEPROC_STATE0, "r+")
-if 'running' in PRUstate.read(7):
-    print("PRU0 is running")
-    PRUstate.close()
-    # Start communication over rpmsg
-    try:
-        PRUdev = open(CHAR_DEV0, "r+", 0)
-        print("Sending message to start communication")
-        PRUdev.write('S')
-        print("Communication established")
-        samp_rate = input("Set sample rate: ")
-        PRUdev.write(samp_rate)
-        num_samp = input("Set number of samples: ")
-        PRUdev.write(num_samp)
-        print("Sample rate & number of samples is set")
-    except:
-        print("Could not open device: 'rpmsg_pru30'")
-        sys.exit()
 
-if 'offline' in PRUstate.read(7):
-    print("PRU0 is offline, starting now")
-    try:
-        PRUstate.write(b'start')
-        print("PRU0 is being started")
+def main():
+    # Initialize PRUs
+    PRUstate = open(REMOTEPROC_STATE0, "r+")
+    if 'running' in PRUstate.read(7):
+        print("PRU0 is running")
         PRUstate.close()
-    except:
-        print("PRU0 failed to start")
-        PRUstate.close()
-        sys.exit()
+        # Start communication over rpmsg
+        try:
+            PRUdev = open(CHAR_DEV0, "r+", 0)
+            print("Sending message to start communication")
+            PRUdev.write('S')
+            print("Communication established")
+            samp_rate = input("Set sample rate: ")
+            PRUdev.write(samp_rate)
+            num_samp = input("Set number of samples: ")
+            PRUdev.write(num_samp)
+            print("Sample rate & number of samples is set")
+        except:
+            print("Could not open device: 'rpmsg_pru30'")
+            sys.exit()
+
+    if 'offline' in PRUstate.read(7):
+        print("PRU0 is offline, starting now")
+        try:
+            PRUstate.write(b'start')
+            print("PRU0 is being started")
+            PRUstate.close()
+        except:
+            print("PRU0 failed to start")
+            PRUstate.close()
+            sys.exit()
 
 
 # Receive several messages over rpmsg
-PRUdev.read()
+
 # Stop PRU
 
 # FFT
 
 # Upload image to imgur
+if __name__ == '__main__':
+    main()
