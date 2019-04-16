c9e88b2d9d8e81fc61396ca31068b7cebad0aea3
SNGomashie <s.gomashie@hotmail.nl>
2019-04-16 15:18:24 +0200

    fix

--- Python/readPRU.py
+++ Python/readPRU.py
@@ -11,7 +11,7 @@ REMOTEPROC_STATE0 = "/sys/class/remoteproc/remoteproc1/state"
 REMOTEPROC_FIRM0 = "/sys/class/remoteproc/remoteproc1/firmware"
 
 # Initialize PRUs
-PRUstate = open(REMOTEPROC_STATE0, "r+", 0)
+PRUstate = open(REMOTEPROC_STATE0, "r+")
 if b'running' in PRUstate.read(7):
     print("PRU0 is running")
     PRUstate.close()
