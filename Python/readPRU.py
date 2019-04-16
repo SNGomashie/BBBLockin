afed5e4a11fe0a1fe55f6d261c04f77776dbf439
SNGomashie <s.gomashie@hotmail.nl>
2019-04-16 15:17:04 +0200

    python fft v0.1

---
+++ Python/readPRU.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python
+
+import sys
+
+
+# Character device PRU0
+CHAR_DEV0 = "/dev/rpmsg_pru30"
+
+# Sysfs interface for PRU0&PRU1
+REMOTEPROC_STATE0 = "/sys/class/remoteproc/remoteproc1/state"
+REMOTEPROC_FIRM0 = "/sys/class/remoteproc/remoteproc1/firmware"
+
+# Initialize PRUs
+PRUstate = open(REMOTEPROC_STATE0, "r+", 0)
+if b'running' in PRUstate.read(7):
+    print("PRU0 is running")
+    PRUstate.close()
+if b'offline' in PRUstate.read(7):
+    print("PRU0 is offline, starting now")
+    try:
+        PRUstate.write(b'start')
+        print("PRU0 is being started")
+        PRUstate.close()
+    except:
+        print("PRU0 failed to start")
+        PRUstate.close()
+        sys.exit()
+
+# Start communication over rpmsg
+try:
+    PRUdev = open(CHAR_DEV0, "r+", 0)
+    PRUdev.write(b'S')
+except:
+    print("Could not open device: 'rpmsg_pru30'")
+    sys.exit()
+
+# Receive several messages over rpmsg
+
+# Stop PRU
+
+# FFT
+
+# Upload image to imgur
