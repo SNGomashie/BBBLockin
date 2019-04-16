a1895329e11e63789991643397df2f006d7315f5
SNGomashie <s.gomashie@hotmail.nl>
2019-04-16 15:33:04 +0200

    fix

--- Python/readPRU.py
+++ Python/readPRU.py
@@ -15,6 +15,21 @@ PRUstate = open(REMOTEPROC_STATE0, "r+")
 if 'running' in PRUstate.read(7):
     print("PRU0 is running")
     PRUstate.close()
+    # Start communication over rpmsg
+    try:
+        PRUdev = open(CHAR_DEV0, "r+", 0)
+        print("Sending message to start communication")
+        PRUdev.write('S')
+        print("Communication established")
+        samp_rate = input("Set sample rate: ")
+        PRUdev.write(samp_rate)
+        num_samp = input("Set number of samples: ")
+        PRUdev.write(num_samp)
+        print("Sample rate & number of samples is set")
+    except:
+        print("Could not open device: 'rpmsg_pru30'")
+        sys.exit()
+
 if 'offline' in PRUstate.read(7):
     print("PRU0 is offline, starting now")
     try:
@@ -26,20 +41,6 @@ if 'offline' in PRUstate.read(7):
         PRUstate.close()
         sys.exit()
 
-# Start communication over rpmsg
-try:
-    PRUdev = open(CHAR_DEV0, "r+", 0)
-    print("Sending message to start communication")
-    PRUdev.write('S')
-    print("Communication established")
-    samp_rate = input("Set sample rate: ")
-    PRUdev.write(samp_rate)
-    num_samp = input("Set number of samples: ")
-    PRUdev.write(num_samp)
-    print("Sample rate & number of samples is set")
-except:
-    print("Could not open device: 'rpmsg_pru30'")
-    sys.exit()
 
 # Receive several messages over rpmsg
 PRUdev.read()
