047eac7016dd25efc04916e188d34ecbbbae5284
SNGomashie <s.gomashie@hotmail.nl>
2019-04-16 17:05:33 +0200

    fix

--- Python/readPRU.py
+++ Python/readPRU.py
@@ -33,7 +33,7 @@ def main():
 
 # Start communication over rpmsg
     try:
-        sleep(1)
+        sleep(5)
         PRUdev = open(CHAR_DEV0, "rb+", 0)
         print("Sending message to start communication")
         PRUdev.write(b'S')
