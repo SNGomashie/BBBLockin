11379e7c548919fea481ebf21ac2610a333b9945
SNGomashie <s.gomashie@hotmail.nl>
2019-04-16 16:36:17 +0200

    debug

--- Hello_world/hello_world.c
+++ Hello_world/hello_world.c
@@ -9,34 +9,38 @@
 /* Used to make sure the Linux drivers are ready for RPMsg communication */
 #define VIRTIO_CONFIG_S_DRIVER_OK	4
 
+#define PIN 7
+
 /* Define the size of message to be recieved. */
 #define RPMSG_BUF_HEADER_SIZE           16
 uint8_t rec_payload[RPMSG_BUF_SIZE - RPMSG_BUF_HEADER_SIZE];
 
 /* Global variable definitions */
 volatile register uint32_t __R31;
+volatile register uint32_t __R30;
 struct pru_rpmsg_transport transport;
 uint16_t src, dst, len;
 
 void main (void) {
   volatile uint8_t *status;
   uint8_t i = 0;
+  __R30 = 0x00000000;
   /*Allow OCP master port access by the PRU so the PRU can read external memories. */
   CT_CFG.SYSCFG_bit.STANDBY_INIT = 0;
 
   /* Make sure the Linux drivers are ready for RPMsg communication. */
   status = &resourceTable.rpmsg_vdev.status;
   while (!(*status & VIRTIO_CONFIG_S_DRIVER_OK));
-
+  __R30 ^= 1 << PIN;
   /* Initialize the RPMsg transport structure */
   while (pru_rpmsg_init(&transport, &resourceTable.rpmsg_vring0, &resourceTable.rpmsg_vring1, TO_ARM_HOST, FROM_ARM_HOST) != PRU_RPMSG_SUCCESS);
-
+  __R30 ^= 1 << PIN;
   /* Create the RPMsg channel between the PRU and ARM user space using the transport structure. */
   while (pru_rpmsg_channel(RPMSG_NS_CREATE, &transport, CHAN_NAME, CHAN_DESC, CHAN_PORT) != PRU_RPMSG_SUCCESS);
-
+  __R30 ^= 1 << PIN;
   /* Receive all available messages, multiple messages can be sent per kick. A message has to be received to set the destination adress before you send. */
   while (pru_rpmsg_receive(&transport, &src, &dst, rec_payload, &len) != PRU_RPMSG_SUCCESS);  //Initialize the RPMsg framework
-
+  __R30 ^= 1 << PIN;
   for(i = 0; i < 200; i++){
   /* Send chars to the ARM, buf = payload, 11 is length of payload. */
     char *buf = "hallo world";
