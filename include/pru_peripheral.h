4a2f303976af192ad3236dff60a20cb359d011ea
SNGomashie <s.gomashie@hotmail.nl>
2019-04-17 16:24:21 +0200

    fix

--- include/pru_peripheral.h
+++ include/pru_peripheral.h
@@ -36,7 +36,7 @@ void IEPstop(void);
 void IEPclear_int(void);
 
 /* enhanced Capture module */
-volatile uint32_t *ecap_period = &CT_ECAP.CAP1;
+// volatile uint32_t *ecap_period = &CT_ECAP.CAP1;
 
 void eCAPintialize(void);
 void eCAPclear_int(void);
