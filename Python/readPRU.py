74324da004da01c28de1bfba2284e94af84ef894
SNGomashie <s.gomashie@hotmail.nl>
2019-04-18 14:19:25 +0200

    utf8

--- Python/readPRU.py
+++ Python/readPRU.py
@@ -37,7 +37,7 @@ def main():
     try:
         PRUdev = open(CHAR_DEV0, "rb+", 0)
         samp_rate = input("Set sample rate: ")
-        PRUdev.write(samp_rate)
+        PRUdev.write(bytes(samp_rate, "utf8"))
         print("Communication established")
         # PRUdev.close()
         # sys.exit()
