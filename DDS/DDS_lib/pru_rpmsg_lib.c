85032bc0796b0b5d5ab38da91fdce724234f6947
SNGomashie <s.gomashie@hotmail.nl>
2019-04-18 17:22:05 +0200

    fix

--- DDS/DDS_lib/pru_rpmsg_lib.c
+++ DDS/DDS_lib/pru_rpmsg_lib.c
@@ -110,7 +110,7 @@ void RPMSGtransmit_block(uint16_t output[248]){
   blk_transmit_status = pru_rpmsg_send(&transport, dst, src, ptrBLK, sizeof(output)/sizeof(uint16_t));
 
   /* See if transmission went correct */
-  while(bkl_transmit_status != PRU_RPMSG_SUCCESS){
+  while(blk_transmit_status != PRU_RPMSG_SUCCESS){
     //    Debugging
     *GPIO1_SET = USR1;
     *GPIO1_SET = USR3;
