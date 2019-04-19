7a3c6729cad452fdbff97b45341bd220f1112fc3
SNGomashie <s.gomashie@hotmail.nl>
2019-04-19 18:11:26 +0200

    10 samples, pickling allowed

--- Python/rpc_readPRU.py
+++ Python/rpc_readPRU.py
@@ -58,9 +58,12 @@ class BeagleBoneDDS(rpyc.Service):
             t.close()
 
     def exposed_pru_read(self):
-        charBuf = self.PRUdev.read(self.RPMSG_BUF_SIZE)
-        intBuf = np.asarray(struct.unpack('<248H', charBuf))
-        return intBuf
+        fullBuf = np.empty(2480)
+        for i in range(10):
+            charBuf = self.PRUdev.read(self.RPMSG_BUF_SIZE)
+            intBuf = np.asarray(struct.unpack('<248H', charBuf))
+            np.append(fullBuf, intBuf)
+        return fullBuf
 
     def exposed_pru_close(self):
         self.PRUstate = open(self.REMOTEPROC_STATE0, "r+")
