22829b678b1e7eb462e528732266455bef5de259
SNGomashie <s.gomashie@hotmail.nl>
2019-04-24 10:16:12 +0200

    save in array

--- DDS/Main.c
+++ DDS/Main.c
@@ -88,10 +88,10 @@ void main(void){
 
       CYCLEstart();
       blkdata[x] = output;
-
+      pru1_mem[0] = CYCLEstop();
       /* add incrementor to phase */
       accumulator += incrementor;
-      pru1_mem[0] = CYCLEstop();
+
       /* Limit the phase accumulator to 24 bits */
       /*       Q00000000.0000000000000000       */
       /*        --------.----------------       */
