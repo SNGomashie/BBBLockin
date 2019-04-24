6cebd0a532f773b249485f9ba3a7af6b8f172017
SNGomashie <s.gomashie@hotmail.nl>
2019-04-24 16:56:10 +0200

    fix

--- McSPI/Main.c
+++ McSPI/Main.c
@@ -39,9 +39,6 @@ void main(void){
   LTC1859initialize();
 
   pru0_mem[0] = LTC1859singletransfer(0);
-  pru0_mem[1] = LTC1859singletransfer(1);
-  pru0_mem[1] = LTC1859singletransfer(2);
-  pru0_mem[1] = LTC1859singletransfer(3);
 
   __halt();
 }
