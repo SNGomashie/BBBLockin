5f624074f51cb92ab2c542999950342f8b345f99
SNGomashie <s.gomashie@hotmail.nl>
2019-04-25 14:50:05 +0200

    clock div 9

--- McSPI/Main.c
+++ McSPI/Main.c
@@ -33,7 +33,7 @@ void main(void){
   PRCMinitialize();
 
   /* Initialize the McSPI module */
-  McSPIinitialze(0x4, 0xF, 0x0);
+  McSPIinitialze(0x9, 0xF, 0x0);
 
   /* Initialize the LTC1859 adc */
   LTC1859initialize();
