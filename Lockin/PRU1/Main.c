be1dd7d5ed05a4fcb363cc11597b08bd04cf7794
SNGomashie <s.gomashie@hotmail.nl>
2019-04-26 14:33:00 +0200

    fix

--- Lockin/PRU1/Main.c
+++ Lockin/PRU1/Main.c
@@ -1,32 +1,24 @@
 #include <stdint.h>
-#include <pru_cfg.h>
+#include <stdio.h>
 #include "resource_table_empty.h"
+#include "pru_peripheral.h"
 
-#define GPIO1 0x4804C000
-#define GPIO_CLEARDATAOUT 0x190
-#define GPIO_SETDATAOUT 0x194
-#define USR0 (1<<21)
-#define USR1 (1<<22)
-#define USR2 (1<<23)
-#define USR3 (1<<24)
-unsigned int volatile * const GPIO1_CLEAR = (unsigned int *) (GPIO1 + GPIO_CLEARDATAOUT);
-unsigned int volatile * const GPIO1_SET   = (unsigned int *) (GPIO1 + GPIO_SETDATAOUT);
-
-volatile register unsigned int __R30;
-volatile register unsigned int __R31;
+#define DEBUG_PIN 7
 
 void main(void) {
-	int i;
+  __R30 = 0x00000000;
 
-	/* Clear SYSCFG[STANDBY_INIT] to enable OCP master port */
-	CT_CFG.SYSCFG_bit.STANDBY_INIT = 0;
-
-	for(i=0; i<5; i++) {
-		*GPIO1_SET = USR2;      // The the USR3 LED on
-		__delay_cycles(500000000/5);    // Wait 1/2 second
-
-		*GPIO1_CLEAR = USR2;
-		 __delay_cycles(500000000/5);
-	}
-	__halt();
+  CT_CFG.SYSCFG_bit.STANDBY_INIT = 0;
+  INTCinitialize(7, 1, 1);
+  IEPinitialize(0x30D3B, 1, cmp);
+  INTERNCOMinitialize(23);
+  IEPstart();
+  while(1){
+    while(__R31 & (1 << 31)){
+      IEPclear_int();
+      INTCclear(7);
+      INTERNCOMpoke();
+    }
+  }
+  __halt();
 }
