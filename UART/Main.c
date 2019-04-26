f0f378680bfea02be15f50e0600b177ab43993b1
SNGomashie <s.gomashie@hotmail.nl>
2019-04-26 10:25:35 +0200

    fix

--- UART/Main.c
+++ UART/Main.c
@@ -3,16 +3,14 @@
 #include "pru_peripheral.h"
 #include "resource_table.h"
 
-
 void main(void){
   char data[16] = "";
-  char data2[16] = "";
 
   UARTinitialize();
 
   while(1){
   __R30 |= (1 << 7);
-    sprintf(data, "%d\n", 0x0000, 0xFFFF, 0xF0F0, 0x0F0F);
+    sprintf(data, "%d\n", 0x0000);
     UARTtransmit(data);
   __R30 &= ~(1 << 7);
     __delay_cycles(200000);
