9a8f181c92280da970bc4da32d47faa3754f407e
SNGomashie <s.gomashie@hotmail.nl>
2019-04-26 11:51:20 +0200

    fix

--- McSPI/Main.c
+++ McSPI/Main.c
@@ -37,10 +37,10 @@ void main(void){
 
   /* Initialize the LTC1859 adc */
   LTC1859initialize();
-  
+
   while(1){
     adc_out = LTC1859readout(0, 1);
-    sprintf(data, "%d", adc_out);
+    sprintf(data, "-  %d\n", adc_out);
     UARTtransmit(data);
   }
 
