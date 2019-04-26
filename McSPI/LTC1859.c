40ca1ce112ce2091f923769f669a0893b1937fbc
SNGomashie <s.gomashie@hotmail.nl>
2019-04-26 09:40:53 +0200

    fix

--- McSPI/LTC1859.c
+++ McSPI/LTC1859.c
@@ -8,86 +8,77 @@
 const uint16_t ADCch[] = {0, 4, 1, 5, 2, 6, 3, 7};
 
 void LTC1859initialize(void){
-while(!(CT_MCSPI0.SYSSTATUS_bit.RESETDONE == 0x1));
+  __R30 = 0x00000000;
+
+  while(!(CT_MCSPI0.SYSSTATUS_bit.RESETDONE == 0x1));
 
   /* Set pins */
-  __R30 |= (1 << CS);
+  __R30 |= (1 << _RD);
   __R30 |= (1 << CONVST);
-  __delay_cycles(150);
-  __R30 &= ~(1 << CONVST);
-  while(!(__R31 & (1 << _BUSY)));
-  __R30 ^= (1 << DEBUG); // HIGH
+  __delay_cycles(100);
 }
 
-uint16_t LTC1859singletransfer(uint8_t chan, uint8_t mode){
+uint16_t LTC1859singletransfer(uint8_t chan){
+  // uint16_t SPIsend = (ADCch[chan] << 12) | 0b0000000000000000; // single-ended, input +/-5V
+  uint16_t SPIsend = (ADCch[chan] << 12) | 0b1000100000000000; // single-ended, input 0V to 5V
+  // uint16_t SPIsend = (ADCch[chan] << 12) | 0b1000010000000000; // single-ended, input +/-10V
+  // uint16_t SPIsend = (ADCch[chan] << 12) | 0b1000110000000000; // single-ended, input 0V to 10V
+  // uint16_t SPIsend = 0b1010100000000000;
+  /* Check if ADC is busy with conversion and continue if not*/
+  while(!(__R31 & (1 << _BUSY)));
+  /* pull down CONVST and _RD */
+  __R30 &= ~(1 << CONVST);
+  __R30 &= ~(1 << _RD);
 
-  __R30 ^= (1 << DEBUG); // LOW
-
-  uint16_t SPIsend = 0;
-  switch(mode){
-    case 0:
-      SPIsend = (ADCch[chan] << 12) | 0b0000000000000000; // single-ended, input +/-5V
-    case 1:
-      SPIsend = (ADCch[chan] << 12) | 0b1000100000000000; // single-ended, input 0V to 5V
-    case 2:
-      SPIsend = (ADCch[chan] << 12) | 0b1000010000000000; // single-ended, input +/-10V
-    case 3:
-      SPIsend = (ADCch[chan] << 12) | 0b1000110000000000; // single-ended, input 0V to 10V
-    default:
-      SPIsend = (ADCch[chan] << 12) | 0b1000100000000000; // single-ended, input 0V to 5V
-  }
-  /* Enable McSPI channel */
+  CT_MCSPI0.CH0CONF_bit.FORCE = 0x1;
   CT_MCSPI0.CH0CTRL_bit.EN = 0x1;
-  __delay_cycles(10);
-  /* Check if McSPI TX register is empty, if it is continue */
-  // while(!(CT_MCSPI0.IRQSTATUS_bit.TX0_EMPTY == 0x1));
 
-  __R30 ^= (1 << DEBUG); // HIGH
+  /* Check if McSPI TX register is empty, if it is continue */
+  while(!(CT_MCSPI0.IRQSTATUS_bit.TX0_EMPTY == 0x1));
 
   /* Write word to be transmitted into TX register */
   CT_MCSPI0.TX0 = SPIsend;
-  __halt();
-  __R30 ^= (1 << DEBUG); // LOW
 
   /* Check if McSPI RX register is full, if it is continue */
   while(!(CT_MCSPI0.IRQSTATUS_bit.RX0_FULL == 0x1));
-  __R30 ^= (1 << DEBUG);
-  /* Disable channel */
-  CT_MCSPI0.CH0CTRL_bit.EN = 0x0;
 
+  CT_MCSPI0.CH0CTRL_bit.EN = 0x0;
   /* Clear interrupts */
   CT_MCSPI0.IRQSTATUS = 0xFFFF;
 
-  /* Start conversion */
+  /* Start conversion, Stop reading */
   __R30 |= (1 << CONVST);
+  // __R30 |= (1 << _RD);
+
+  /* Delay until conversion starts */
   __delay_cycles(100);
   __R30 &= ~(1 << CONVST);
-
-  /* Wait until conversion is done */
+  /* Wait until Conversion is done */
   while(!(__R31 & (1 << _BUSY)));
 
-  /* Enable McSPI channel */
-  CT_MCSPI0.CH0CTRL_bit.EN = 0x1;
+  /* pull down CONVST and _RD */
 
+  __R30 &= ~(1 << _RD);
+
+  CT_MCSPI0.CH0CTRL_bit.EN = 0x1;
   /* Write nothing to TX Register */
   CT_MCSPI0.TX0 = 0x0000;
 
   /* Wait until RX register is full */
   while(!(CT_MCSPI0.IRQSTATUS_bit.RX0_FULL == 0x1));
 
-  /* Disable McSPI channel */
   CT_MCSPI0.CH0CTRL_bit.EN = 0x0;
+
+  CT_MCSPI0.CH0CONF_bit.FORCE = 0x0;
 
   /* Clear interrupts */
   CT_MCSPI0.IRQSTATUS = 0xFFFF;
 
-  /* Start conversion */
-  __R30 |= (1 << CONVST);
-  __delay_cycles(100);
-  __R30 &= ~(1 << CONVST);
 
-  /* Wait until conversion is done */
-  while(!(__R31 & (1 << _BUSY)));
+
+  /* Start conversion */
+  // __R30 |= (1 << CONVST);
+  // __R30 |= (1 << _RD);
 
   return CT_MCSPI0.RX0;
 }
