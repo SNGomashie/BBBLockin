8ae2cf653d8aaa7fcd98c3f5c258f5b026d390be
SNGomashie <s.gomashie@hotmail.nl>
2019-04-29 17:31:23 +0200

    Lock-in implementation v0.2

--- Lockin/PRU0/Main.c
+++ Lockin/PRU0/Main.c
@@ -1,15 +1,22 @@
 #include <stdint.h>
+#include <stdlib.h>
 #include <stdio.h>
 #include "pru_peripheral.h"
 #include "pru_rpmsg_lib.h"
 
-#define DEBUG_PIN 7
+/* PIN number used for debugging */
+#define DEBUG_PIN 3
 
 void main(void) {
-  /* Clear all outputs */
-  __R30 = 0x00000000;
-  /*Allow OCP master port access by the PRU so the PRU can read external memories. */
-  CT_CFG.SYSCFG_bit.STANDBY_INIT = 0;
+  /************************/
+  /*        SETUP         */
+  /************************/
+  __R30 = 0x00000000;  // Clear al output pins
+  CT_CFG.SYSCFG_bit.STANDBY_INIT = 0;  // Allow OCP master port access by the PRU so the PRU can read external memories.
+  /************************/
+  /************************/
+
+
 
   /*************************/
   /* Variable declarations */
@@ -19,21 +26,26 @@ void main(void) {
   uint16_t uint16Freq = 0;  // Desired sample frequency
   uint32_t uint32Period = 0;  // sample frequency Hz converted to S
 
-  uint8_t uint8Num_samples = 0;  // Number of samples wanted
+  uint8_t uint8packets = 0;  // Number of samples wanted
 
-  uint16_t uint16ADC = 0;
+  uint16_t uint16ADC = 0;  // ADC output
+  uint16_t uint16Cos = 0;  // Cos output DDS
+  uint16_t uint16Sin = 0;  // Sin output DDS
+
+  uint32_t uint32Q, uint32I, uint32R = 0;  // Quadrature, In-phase and Magnitude
+  uint64_t uint64Qpow, uint64Ipow = 0;
+  uint16_t uint16Navr = 1000;  // Integration time
   /*************************/
   /*************************/
 
 
 
-  /***********************/
-  /*   Initializations   */
-  /***********************/
+  /*************************/
+  /*    Initializations    */
+  /*************************/
   RPMSGinitialize();  // Initialize remoteproc messaging framework
   INTCinitialize(7, 1, 1);  // Initialize interrupt controller | sys_evt 7, channel 1, host_int 1
-  INTERNCOMinitialize(25);  // Initialize internal communcation | sys_ect 25
-  eCAPinitialize();  // Initialize enhanced capture module
+  INTERNCOMinitialize(25);  // Initialize internal communcation | sys_evt 25
   PRCMinitialize();  // Initialize clock for McSPI
   McSPIinitialze(0x5, 0xF, 0x0);  // Initialize McSPI module | 1,5MHz clk, 16bit word, no intterrupts
   LTC1859initialize();  // Initialize LTC1859 adc
@@ -43,24 +55,95 @@ void main(void) {
   uint32Period = (1000000000 / uint16Freq) / 5;  // Convert freq int to period int
 
   IEPinitialize(uint32Period, 1, cmp);  // Initialize IEP timer | received period, increment by 1, compare mode
-  /***********************/
-  /***********************/
+  /*************************/
+  /*************************/
 
 
 
-  /***********************/
-  /*     Main program    */
-  /***********************/
+  /*************************/
+  /*      Main program     */
+  /*************************/
   INTERNCOMpoke();  // Wake up PRU1
+  sRAM[3] = uint32Period / 100;
   IEPstart();  // Start IEP timer
 
-  /*      Main loop      */
+
+  /*       Main loop       */
   while(1){
-    while(__R31 & (1 << 31)){  // Interrupt polling
+    while(__R31 & (1 << 31)){  // IEP interrupt polling
       IEPclear();  // Clear IEP cmp register and system event
-      uint16ADC = LTC1859readout(0, 1);
-      
+
+      uint16ADC = LTC1859readout(0, 1);  // Read a sample form the LTC1859
+
+      INTERNCOMlisten();  // Wait for DDS to be done on PRU1
+      INTERCOMpoke();  // Respond to PRU1
+
+
+      /* TODO: is this gonna be shared memory or scratchpad? */
+      uint16Sin = sRAM[0] & 0xFFFF0000;  // SIN is 16 MSB of sRAM reg 0
+      uint16Cos = sRAM[0] & 0x0000FFFF;  // COS is 16 LSB of sRAM reg 0
+
+      /* Quadrature calculation and moving average filtering */
+      uint32Q -= uint32Q / uint16Navr;
+      uint32Q += (uint16Sin * uint16ADC) / uint16Navr;
+
+      /* In-phase calculation and moving average filtering */
+      uint32I -= uint32I / uint16Navr;
+      uint32I += (uint16Cos * uint16ADC) / uint16Navr;
+
+      /* Magnitude calculation and moving avergae filtering */
+      uint32R -= uint32R / uint16Navr;
+
+      uint64Qpow = (uint64)uint32Q * (uint64)uint32Q;  // Calculate Q sqaured using MAC
+      uint64Ipow = (uint64)uint32I * (uint64)uint32I;  // Calculate I squared using MAC
+
+      uint32R += sqrt(uint64Qpow + uint64Ipow) / uint16Navr;  // Magnitude calculation
+
+      if(RPMSGcollect_send(uint32R) == uint8packets){
+        IEPstop();
+      }
     }
   }
+ /************************/
+
+
   __halt();
 }
+ /************************/
+ /************************/
+
+
+
+ uint16_t sqrt(uint32_t x){
+ 	uint32_t fa,sa,ta;
+ 	uint32_t error,error_last;
+
+   // Since the squareroot of X will always be smaller than X / 2
+ 	// We can choose an (arbitrary) first approach as X / 2
+ 	// The closer this number is to the final answer the faster this routines completes.
+
+ 	fa = x>>1;		// Divide number by 2, bitshifting performs the same operation
+
+ 	// Divide the argument number by the first approach
+ 	sa = x/fa;
+
+ 	// Get the mean between the two previous numbers (add them and divide by 2).
+ 	ta = (fa+sa)>>1;
+
+ 	error_last=-1;
+ 	error=0;
+
+ 	/*
+ 	 * Repeat this routine until the integer output value is no longer changing.
+ 	 */
+ 	do {
+ 		error_last=error;
+ 		fa = ta;
+ 		sa = x/fa;
+ 		ta = (fa+sa)>>1;
+ 		error=iabs(x - ta*ta);
+ 	} while (error_last!=error);
+
+ 	// Return the integer result: square root of the input argument x.
+ 	return ta;
+ }
--- Lockin/PRU1/Main.c
+++ Lockin/PRU1/Main.c
@@ -1,15 +1,65 @@
 #include <stdint.h>
+#include <stdlib.h>
 #include <stdio.h>
-#include "resource_table_empty.h"
 #include "pru_peripheral.h"
+#include "am335x_pru_dds.h"
 
-#define DEBUG_PIN 7
+/* Pin number used for debugging */
+#define DEBUG_PIN 10
 
 void main(void) {
+  /************************/
+  /*        SETUP         */
+  /************************/
+  __R30 = 0x00000000;  // Clear al output pins
+  CT_CFG.SYSCFG_bit.STANDBY_INIT = 0;  // Allow OCP master port access by the PRU so the PRU can read external memories.
+  /************************/
+  /************************/
+
+
+
+  /*************************/
+  /* Variable declarations */
+  /*************************/
+  uint32_t *uint32Sample_period = &sRAM[3];
+  /* TODO: Change to using scratchpad */
+  uint32_t uint32Sample_amount = 0;
+
+  struct DDS ddsReference;
+  /*************************/
+  /*************************/
+
+
+
+  /*************************/
+  /*    Initializations    */
+  /*************************/
+  eCAPinitialize();  // Initialize enchanced capture module
+
+  INTERNCOMlisten();  // Wait until sample period is loaded into shared memory
+
+  DDSinitialize(&ddsReference, *uint32Sample_period);  // Initialize Numerical Oscillator
+  /*************************/
+  /*************************/
+
+
+
+  /*************************/
+  /*      Main program     */
+  /*************************/
+
+
+  /*       Main loop       */
   while(1){
-    while(__R31 & (1 << 31)){
-      __R30 ^= (1 << DEBUG_PIN);
-      while(!(CT_IEP.TMR_CMP_STS == 0));
+    while(__R31 & (1 << 31)){   // IEP interrupt polling
+      while(!(CT_IEP.TMR_CMP_STS == 0));  // Wait until PRU0 has cleared the interrupt
+        DDSsetfreq(&ddsReference);  // Change the tuning word to stay in-phase
+        DDSstep(&ddsReference);  // Go to the next value of the sin wave
+
+        /* TODO: over scratchpad or shared memory?? */
+
+        INTERNCOMpoke();
+        INTERNCOMlisten();
     }
   }
   __halt();
