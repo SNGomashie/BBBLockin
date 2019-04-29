afc32a9638485af89fb2388f9f64c1fef6573793
SNGomashie <s.gomashie@hotmail.nl>
2019-04-29 11:12:54 +0200

    fix

--- Lockin/PRU1/Main.c
+++ Lockin/PRU1/Main.c
@@ -1,25 +1,15 @@
 #include <stdint.h>
 #include <stdio.h>
-#include <pru_cfg.h>
-#include <pru_ctrl.h>
 #include "resource_table_empty.h"
 #include "pru_peripheral.h"
 
-#define DEBUG_PIN 5
+#define DEBUG_PIN 7
 
 void main(void) {
-  __R30 = 0x00000000;
-
-  CT_CFG.SYSCFG_bit.STANDBY_INIT = 0;
-  INTCinitialize(7, 1, 1);
-  IEPinitialize(0x30D40, 1, cmp);
-  IEPstart();
-
   while(1){
     while(__R31 & (1 << 31)){
-      IEPclear_int();
-      INTCclear(7);
       __R30 ^= (1 << DEBUG_PIN);
+      while(!(CT_IEP.TMR_CMP_STS == 0));
     }
   }
   __halt();
