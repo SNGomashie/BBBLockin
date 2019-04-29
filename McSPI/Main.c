133c5aa40080efcf62cfd1fc740383c5ef48863b
SNGomashie <s.gomashie@hotmail.nl>
2019-04-29 10:47:31 +0200

    McSPI rpmsg

--- McSPI/Main.c
+++ McSPI/Main.c
@@ -15,31 +15,33 @@
 #include <stdio.h>
 #include "pru_peripheral.h"
 #include "LTC1859.h"
-#include "resource_table.h"
+#include "pru_rpmsg_lib.h"
 
 #define HOST_INT (1 << 31)
 
 #define PRU0_MEM 0x00000000
 volatile uint32_t *pru0_mem =  (unsigned int *) PRU0_MEM;
 
+uint16_t blkdata[248];
+
 void main(void){
   uint16_t adc_out = 0;
-  char data[16] = "";
+  char* RPMsg_in;
+
   /* Clear output register */
   __R30 = 0x00000000;
 
   /* Clear SYSCFG[STANDBY_INIT] to enable OCP master port */
   CT_CFG.SYSCFG_bit.STANDBY_INIT = 0;
 
+  RPMSGinitialize();
+  INTCinitialize(7, 1, 1);
   /* Access PRCM (without CT) to initialize McSPI0 clock */
   PRCMinitialize();
-  UARTinitialize();
   /* Initialize the McSPI module */
   McSPIinitialze(0x5, 0xF, 0x0);
-
   /* Initialize the LTC1859 adc */
   LTC1859initialize();
-  INTCinitialize(7, 1, 1);
   IEPinitialize(20000, 1, cmp);
   IEPstart();
 
@@ -47,15 +49,22 @@ void main(void){
     while(__R31 & HOST_INT){
       IEPclear_int();
       INTCclear(7);
-    // CYCLEstart();
-    adc_out = LTC1859readout(0, 1);
-    // pru0_mem[0] = CYCLEstop();
+      adc_out = LTC1859readout(0, 1);
+      blkdata[x] = adc_out;
+      x++;
+      if(!(x < 248)){
+        RPMSGtransmit_block(blkdata);
+        x = 0;
+        i++;
+        if(!(i < amount_samp)){
+          IEPstop();
+        }
+      }
     // sprintf(data, "%d\n", adc_out);
     // UARTtransmit(data);
     // __delay_cycles(0x1312D00);
     }
   }
-
 
   __halt();
 }
