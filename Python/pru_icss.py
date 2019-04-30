3f8a5d0c89a1a25583876eceb9c9d21ea77a02fe
SNGomashie <s.gomashie@hotmail.nl>
2019-04-30 11:34:24 +0200

    python update

---
+++ Python/pru_icss.py
@@ -0,0 +1,83 @@
+import struct
+import time
+import numpy as np
+
+
+class PRU_ICSS:
+    # Class atributes
+    RPMSG_BUF_SIZE = 512
+    charBuf = "\0" * 512
+
+    def __init__(self, pru):
+        self.pru = pru
+        if self.pru == 0:
+            self.CHAR_DEV = "/dev/rpmsg_pru30"
+            self.STATE = "/sys/class/remoteproc/remoteproc1/state"
+        elif self.pru == 1:
+            self.CHAR_DEV = "/dev/rpmsg_pru31"
+            self.STATE = "/sys/class/remoteproc/remoteproc2/state"
+        self.PRU_status = open(self.STATE, "r+")
+
+    def status(self):
+        state = self.PRU_status.read(7)
+        return state
+
+    def start(self):
+        if 'running' in self.status():
+            print("-    PRU%d is running" % (self.pru))
+        elif 'offline' in self.status():
+            print("-    PRU%d is offline, starting now" % (self.pru))
+            try:
+                self.PRU_status.write('start')
+                print("-    PRU%d has started" % (self.pru))
+                time.sleep(2)
+            except IOError:
+                print("-  ERROR  PRU%d failed to start" % (self.pru))
+
+    def stop(self):
+        if 'offline' in self.status():
+            print("-    PRU%d is offline" % (self.pru))
+        elif 'offline' in self.status():
+            print("-    PRU%d is running, stopping now" % (self.pru))
+            try:
+                self.PRU_dev.close()
+                print("-    Closing character device")
+                # Delay for a few seconds so the kernel can flush the remoteproc FIFO
+                time.sleep(2)
+                self.PRU_status.write('stop')
+                print("-    PRU%d has stopped" % (self.pru))
+                self.PRU_status.close()
+                time.sleep(2)
+            except IOError:
+                print("-  ERROR  PRU%d failed to stop" % (self.pru))
+                self.PRU_status.close()
+
+    def transmit(self, message):
+        try:
+            self.PRU_dev = open(self.CHAR_DEV, "rb+", 0)
+            self.PRU_dev.write(bytes(str(message), 'ASCII'))
+            print('-    "%s" transmitted to PRU%d' % (message, self.pru))
+        except IOError:
+            print("-  ERROR  Could not open device: 'rpmsg_pru30'")
+            t.close()
+        pass
+
+    def blkreceive(self, samples, data_size):
+        fullBuf = np.empty(0)
+        div = data_size / 8
+        num = 496 / div
+        tot = np.ceil(samples / num)
+        self.PRU_dev.write(bytes(str(tot), 'ASCII'))
+        print("-    %d samples will be transfered in %d packets" % (samples, tot))
+        for i in range(np.uint16(tot)):
+            charBuf = self.PRU_dev.read(self.RPMSG_BUF_SIZE)
+            intBuf = np.asarray(struct.unpack('<248H', charBuf))
+            fullBuf = np.append(fullBuf, intBuf)
+            print("\r-    RPMsg packet received ( %d / %d )" % ((i + 1), tot), end='')
+        return fullBuf
+
+    def receive(self):
+        charBuf = self.PRU_dev.read(self.RPMSG_BUF_SIZE)
+        intBuf = np.asarray(struct.unpack('<248H', charBuf))
+        print("-    Packter received from PRU%d" % (self.pru))
+        return intBuf
--- Python/pru_rpc.py
+++ Python/pru_rpc.py
@@ -1,8 +1,5 @@
 import rpyc
-import struct
-import time
-import numpy as np
-import sys
+from pru_icss import PRU_ICSS
 
 rpyc.core.protocol.DEFAULT_CONFIG['allow_pickle'] = True
 
@@ -22,83 +19,52 @@ class BeagleBoneDDS(rpyc.Service):
         pass
 
     def exposed_pru_initialize(self, pru):
-        self.pru = pru
         if pru == 0:
-            self.CHAR_DEV = "/dev/rpmsg_pru30"
-            self.STATE = "/sys/class/remoteproc/remoteproc1/state"
+            self.PRU0 = PRU_ICSS(0)
+        if pru == 1:
+            self.PRU1 = PRU_ICSS(1)
+        if pru == 2:
+            self.PRU0 = PRU_ICSS(0)
+            self.PRU1 = PRU_ICSS(1)
+        pass
+
+    def exposed_pru_start(self, pru):
+        if pru == 0:
+            self.PRU0.start()
+        if pru == 1:
+            self.PRU1.start()
+        if pru == 2:
+            self.PRU0.start()
+            self.PRU1.start()
+        pass
+
+    def exposed_pru_stop(self, pru):
+        if pru == 0:
+            self.PRU0.stop()
+        if pru == 1:
+            self.PRU1.stop()
+        if pru == 2:
+            self.PRU0.stop()
+            self.PRU1.stop()
+        pass
+
+    def exposed_pru_transmit(self, message, pru):
+        if pru == 0:
+            self.PRU0.transmit(message)
+        if pru == 1:
+            self.PRU1.transmit(message)
+
+    def exposed_pru_blk_receive(self, samples, data_size, pru):
+        if pru == 0:
+            return self.PRU0.blkreceive(samples, data_size)
         elif pru == 1:
-            self.CHAR_DEV = "/dev/rpmsg_pru31"
-            self.STATE = "/sys/class/remoteproc/remoteproc2/state"
+            return self.PRU1.blkreceive(samples, data_size)
 
-        self.PRUstate = open(self.STATE, "r+")
-
-    def exposed_pru_start(self):
-        state = self.PRUstate.read(7)
-        if 'running' in state:
-            print("-    PRU%d is running" % (self.pru))
-
-        elif 'offline' in state:
-            print("-    PRU%d is offline, starting now" % (self.pru))
-            try:
-                self.PRUstate.write('start')
-                print("-    PRU%d has started" % (self.pru))
-                self.PRUstate.close()
-                time.sleep(2)
-            except IOError:
-                print("-  ERROR  PRU%d failed to start" % (self.pru))
-                self.PRUstate.close()
-                t.close()
-        pass
-
-    def exposed_pru_stop(self):
-        state = self.PRUstate.read(7)
-        if 'offline' in state:
-            print("-    PRU%d is offline" % (self.pru))
-
-        elif 'offline' in state:
-            print("-    PRU%d is running, stopping now" % (self.pru))
-            try:
-                self.PRUdev.close()
-                # Delay for a few seconds so the kernel can flush the remoteproc FIFO
-                time.sleep(2)
-                self.PRUstate.write('stop')
-                print("-    PRU%d has stopped" % (self.pru))
-                self.PRUstate.close()
-                time.sleep(2)
-            except IOError:
-                print("-  ERROR  PRU%d failed to stop" % (self.pru))
-                self.PRUstate.close()
-                t.close()
-        pass
-
-    def exposed_pru_transmit(self, message):
-        try:
-            self.PRUdev = open(self.CHAR_DEV, "rb+", 0)
-            self.PRUdev.write(bytes(str(message), 'ASCII'))
-            print('-    "%s" transmitted to PRU%d' % (message, self.pru))
-        except IOError:
-            print("-  ERROR  Could not open device: 'rpmsg_pru30'")
-            t.close()
-        pass
-
-    def exposed_pru_blk_receive(self, samples):
-        fullBuf = np.empty(0)
-        tot = np.ceil(samples / 248)
-        self.PRUdev.write(bytes(str(tot), 'ASCII'))
-        print("-    %d samples will be transfered in %d packets" % (samples, tot))
-        for i in range(np.uint16(tot)):
-            charBuf = self.PRUdev.read(self.RPMSG_BUF_SIZE)
-            intBuf = np.asarray(struct.unpack('<248H', charBuf))
-            fullBuf = np.append(fullBuf, intBuf)
-            print("\r-    RPMsg packet received ( %d / %d )" % ((i + 1), tot), end='')
-        return fullBuf
-
-    def exposed_pru_receive(self):
-        charBuf = self.PRUdev.read(self.RPMSG_BUF_SIZE)
-        intBuf = np.asarray(struct.unpack('<248H', charBuf))
-        print("-    One RPMsg packet received")
-        return intBuf
-
+    def exposed_pru_receive(self, pru):
+        if pru == 0:
+            return self.PRU0.receive()
+        elif pru == 1:
+            return self.PRU0.receive()
 
 
 if __name__ == '__main__':
