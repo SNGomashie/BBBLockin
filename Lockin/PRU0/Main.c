94f436a4e73b7f4582e8c913edaee03f27ec2013
SNGomashie <s.gomashie@hotmail.nl>
2019-05-01 12:59:09 +0200

    debug

--- Lockin/PRU0/Main.c
+++ Lockin/PRU0/Main.c
@@ -109,6 +109,9 @@ void main(void) {
   while(1){
     while(__R31 & (1 << 31)){  // IEP interrupt polling
       IEPclear();  // Clear IEP cmp register and system event
+
+      __R30 ^= DEBUG_PIN;
+
       int16ADC = LTC1859readout(0, 0);  // Read a sample form the LTC1859
 
       INTERNCOMlisten(1, PRU1_PRU0_SEND_INT);  // Wait for NCO to be done on PRU1
@@ -135,6 +138,7 @@ void main(void) {
       if(RPMSGcollect16_send(int16ADC) == uint16packets){
         IEPstop();
       }
+      __R30 ^= DEBUG_PIN;
     }
   }
  /************************/
