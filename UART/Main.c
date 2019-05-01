20ce5ae47fffd0893e4ee304e6ae813eddcc3e3f
SNGomashie <s.gomashie@hotmail.nl>
2019-05-01 14:33:59 +0200

    fix

--- UART/Main.c
+++ UART/Main.c
@@ -5,9 +5,9 @@
 
 void main(void){
   char data[16] = "";
-  uint32_t c = 0;
-  int16_t a = -10;
-  uint16_t b = 10;
+  int32_t c = 0;
+  int16_t a = -32768;
+  uint16_t b = 65535;
   c = a * b;
   UARTinitialize();
   sprintf(data, "%d\n", c);
