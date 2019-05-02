e54f396bb5549fcee29f8c6da3f81b6b7963711b
SNGomashie <s.gomashie@hotmail.nl>
2019-05-02 10:43:56 +0200

    fix

--- UART/Main.c
+++ UART/Main.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 #include <stdint.h>
+#include <math.h>
 #include "pru_peripheral.h"
 #include "resource_table.h"
 
 void main(void){
   char data[16] = "";
-  int32_t c = 0;
-  int16_t a = -32768;
-  uint16_t b = 65535;
-  c = a * b;
+  uint32_t c = 0;
+  uint64_t b = ‭2147483648‬;
+  c = sqrt(b);
   UARTinitialize();
   sprintf(data, "%d\n", c);
   UARTtransmit(data);
