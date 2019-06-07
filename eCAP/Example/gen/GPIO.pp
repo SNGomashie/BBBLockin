f03ab88c05ef5e3923fea103a1bf3134ec113290
Sena Gomashie <40298891+SNGomashie@users.noreply.github.com>
2019-06-07 10:12:03 +0200

    Remove eCAP generated files

--- eCAP/Example/gen/GPIO.pp
+++
@@ -1,7 +0,0 @@
-gen/GPIO.object: GPIO.c
-gen/GPIO.object: GPIO.h
-gen/GPIO.object: /usr/share/ti/cgt-pru/include/stdio.h
-gen/GPIO.object: /usr/share/ti/cgt-pru/include/linkage.h
-gen/GPIO.object: /usr/share/ti/cgt-pru/include/stdarg.h
-gen/GPIO.object: /usr/share/ti/cgt-pru/include/stdint.h
-gen/GPIO.object: /usr/lib/ti/pru-software-support-package/include/am335x/pru_cfg.h
