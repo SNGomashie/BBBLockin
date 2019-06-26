1d6a024180b3a1c9f5c3e5d90cc65ca76f49c58e
Sena Gomashie <40298891+SNGomashie@users.noreply.github.com>
2019-06-26 12:05:24 +0200

    filters updated

--- Python/Simulations/Filter.py
+++ Python/Simulations/Filter.py
@@ -1,19 +1,34 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy import signal, fftpack
+from scipy import signal
 
 
-def ma():
-    Fs = 10000
-    cutoff = 0.1
+def main():
+    Fs = 10000  # Sample frequency in Hz
+    cutoff = 0.1  # Cutoff requency in Hz
     nyq = 0.5 * Fs
 
-    N = 40000
+    N = 40000  # Number of taps in FIR filter
+    order = 6  # Order number of IIR filter
+    passband = 1
+    stopband = 40
 
+    ''' Generate coëfficients for FIR Filter
+        firwin(num of taps, normalized cutoff, window type)
+        butter(filter order, normalized cutoff, filter type, analog=False)
+        cheby1(filter order, passband ripple, cutoff frequency, filter tpye, sample frequency)
+        cheby2(filter order, stopband attenuation, cutoff frequency, filter type, sample frequency)
+        ellip(filter order, passband ripple, stopband attenuation, cutoff frequency, filter type, sample frequency)
+    '''
     taps = signal.firwin(N, cutoff/nyq, window=('hamming'))
+    # b, a = signal.butter(order, cutoff/nyq, btype='low', analog=False)
+    # b, a = signal.cheby1(order, passband, cutoff, 'lp', fs=Fs)
+    # b, a = signal.cheby2(order, stopband, cutoff, 'lp', fs=Fs)
+    # b, a = signal.ellip(order, passband, stopband, cutoff, 'lp', fs=Fs)
 
-    w, h = signal.freqz(taps, 1.0, worN=20000)
+    w, h = signal.freqz(taps, 1.0, worN=20000)  # Compute the frequency response
 
+    # Draw freuency plot
     plt.figure()
     plt.plot(nyq*w/np.pi, 20 * np.log10(np.abs(h)), 'b')
     plt.xlim(0, 10)
@@ -22,8 +37,6 @@ def ma():
     plt.xlabel('Frequency [Hz]')
     plt.ylabel('Attenuation [dB]')
     plt.grid()
-
-
 
 
 if __name__ == "__main__":
